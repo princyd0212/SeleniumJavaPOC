c206a8a3e86fd1bc1c625a30e9f01b01cc8f159c
Samir Patel <samir.p@tridhya.com>
2024-07-09 17:18:00 +0530

    Change

--- src/main/java/Create_Maven_Project_18/StandAloneTest.java
+++ src/main/java/Create_Maven_Project_18/StandAloneTest.java
@@ -2,12 +2,9 @@ package Create_Maven_Project_18;
 
 import Page_Object_Factory_19.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
