6629c07e0689a8f3850edcc8bdeebebe195b7b8c
Samir Patel <samir.p@tridhya.com>
2024-07-29 18:30:08 +0530

    updated code of cucumber

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -8,6 +8,7 @@ import io.cucumber.testng.CucumberOptions;
         features = "src/test/java/cucumber",
         glue = "stepDefinations",
         monochrome = true,
+//        tags = "@Regression",
         plugin = {"html:target/cucumber.html"}
 )
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
