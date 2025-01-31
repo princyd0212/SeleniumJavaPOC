8b0f9e626584969dcade574e2ae66a98ae3ef431
laharns <lahar.innvoius@gmail.com>
2025-01-31 11:06:46 +0530

    Scroll data code added

--- src/main/java/com/sam/selenium/pageObjects/CheckoutPage.java
+++ src/main/java/com/sam/selenium/pageObjects/CheckoutPage.java
@@ -2,6 +2,7 @@ package com.sam.selenium.pageObjects;
 
 import com.sam.selenium.AbstractComponents.AbstractComponent;
 import com.sam.selenium.utils.PropertyFileReader;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import java.io.IOException;
@@ -23,10 +24,14 @@ public class CheckoutPage extends AbstractComponent {
         a.sendKeys(driver.findElement(propertyFileReader.getLocator("checkoutPage.country")), countryName).build().perform();
         waitForElementToAppear(propertyFileReader.getLocator("checkoutPage.results"));
         clickElement(propertyFileReader.getLocator("checkoutPage.selectAnyCountry"));
+
     }
 
     public ConfirmationPage submitOrder() throws IOException {
-        clickElement(propertyFileReader.getLocator("checkoutPage.submitButton"));
+        By submitButton = propertyFileReader.getLocator("checkoutPage.submitButton");
+        scrollToElement(submitButton);
+        waitForElementToAppear(submitButton);
+        clickElement(submitButton);
         return new ConfirmationPage(driver);
     }
 
