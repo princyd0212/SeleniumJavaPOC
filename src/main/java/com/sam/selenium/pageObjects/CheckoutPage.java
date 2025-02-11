91e57ad81026b18738774319009c42e05bdf048b
KRBrahmbhatt <kaushalbrahmbhattt@gmail.com>
2025-02-11 11:30:15 +0530

    Change in scroll of checkout page

--- src/main/java/com/sam/selenium/pageObjects/CheckoutPage.java
+++ src/main/java/com/sam/selenium/pageObjects/CheckoutPage.java
@@ -4,6 +4,7 @@ import com.sam.selenium.AbstractComponents.AbstractComponent;
 import com.sam.selenium.utils.PropertyFileReader;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import java.io.IOException;
 
@@ -29,7 +30,7 @@ public class CheckoutPage extends AbstractComponent {
 
     public ConfirmationPage submitOrder() throws IOException {
         By submitButton = propertyFileReader.getLocator("checkoutPage.submitButton");
-        scrollToElement(submitButton);
+        scrollToElement((WebElement) submitButton);
         waitForElementToAppear(submitButton);
         clickElement(submitButton);
         return new ConfirmationPage(driver);
